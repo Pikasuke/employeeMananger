e16d4d6134c1f8b9e13669befb22e57dd6954e22
pikasuke <tongfongctm@hotmail.com>
2022-11-07 23:00:58 +0100

    properties DB connexion

--- src/main/java/com/moi/employeManager/Controllers/EmployeControllers.java
+++ src/main/java/com/moi/employeManager/Controllers/EmployeControllers.java
@@ -5,9 +5,7 @@ import com.moi.employeManager.Service.EmployeService;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +24,10 @@ public class EmployeControllers {
     public ResponseEntity<List<Employee>> getAllEmployes() {
         List<Employee> employees = employeService.findAllEmployes();
         return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public Employee addEmploye (@RequestBody Employee employee) {
+        return employeService.addEmploye(employee);
     }
 }
